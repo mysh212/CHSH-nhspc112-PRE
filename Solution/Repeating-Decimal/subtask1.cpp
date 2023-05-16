dce39a3f68d69710b822a5b9b8a4702d3e181911
Gamic1234 <121374262+Gamic1234@users.noreply.github.com>
2023-05-16 14:20:20 +0800

    Update subtask1.cpp

--- Solution/Repeating-Decimal/subtask1.cpp
+++ Solution/Repeating-Decimal/subtask1.cpp
@@ -1,29 +1,15 @@
 #include<bits/stdc++.h>
-#define endl '\n'
-#define OuO ios::sync_with_stdio(0);cin.tie(0);
-#define ll long long
 using namespace std;
- 
-void solve(){
-    int p, q, len = 0;
-    cin >> p >> q;
-    set<int> se;
-    p = p % q;
-    while(!se.count(p)){
-        se.insert(p);
-        p *= 10;
-        while(p < q){
-            p *= 10;
-            ++len;
-        }
-        len += (int)log10(p / q) + 1;
-        p %= q;
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+    vector<int> ans = {0,0,0,1,0,0,1,6,0,1,0};
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int b, c;
+        cin >> b >> c;
+        cout << ans[c] << endl;
     }
-    cout << len << endl;
-}
- 
-signed main(){OuO
-    int tt;
-    cin >> tt;
-    while(tt--) solve();
+    return 0;
 }
